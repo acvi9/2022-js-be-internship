220ae4d56cd53854a1a13b6189f35d286b61c77f
NikolaHromcik <nhromcik99@gmail.com>
2022-10-27 09:14:00 +0200

    application setup/hello world in express

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require("express")
+const app = express()
+const port = 3000
+
+app.get('/', (req,res)=>{
+    res.send("Hello world!")
+})
+
+app.listen(port)
+console.log(`Express app is running on localhost:${port}`)
