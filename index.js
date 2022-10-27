d6d8ed72487fdf7805cba0034fd032e771397c9f
Andrija Stanković <74971935+AkosoCodes@users.noreply.github.com>
2022-10-27 12:13:25 +0200

    Added ;

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const express = require("express")
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = 3000;
 
 app.get('/', (req,res)=>{
-    res.send("Hello world!")
+    res.send("Hello world!");
 })
 
-app.listen(port)
-console.log(`Express app is running on localhost:${port}`)
+app.listen(port);
+console.log(`Express app is running on localhost:${port}`);
