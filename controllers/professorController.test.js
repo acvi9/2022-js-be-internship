819513fbc1921042cd5564c472a1fcd5cd83aa1d
Andrija Stankovic <andrijas0721@gmail.com>
2022-11-02 15:17:31 +0100

    Formatted Test

--- controllers/professorController.test.js
+++ controllers/professorController.test.js
@@ -1,36 +1,22 @@
-const request = require('supertest')
+const request = require('supertest');
 
-test('should first', async () => {
-    await request('localhost:3000')
-    .get('/list/professors')
-    .then(data =>{
-        console.log(data.statusCode)
-        expect(data.statusCode).toBe(200)
-    })
-})
+describe('Professor Controller', () => {
 
+    test('Should test if the connection to the route is successful', async () => {
+        await request('localhost:3000')
+        .get('/list/professors')
+        .then(data =>{
+            console.log(data.statusCode)
+            expect(data.statusCode).toBe(200)
+        })
+    });
 
-
-// const SequelizeMock = require('sequelize-mock');
-// const dbMock = new SequelizeMock();
-// const professorModel = require('../models/professorModel');
-
-// const ProfessorMock = dbMock.define('professor', {
-//     name: 'Andrija',
-//     surname: 'Stankovic',
-//     email: 'andrija@gmail.com',
-//     password: '123456'}, {
-//     instanceMethods: {
-//         listAllProfessors: () => {
-//             return ProfessorMock.findAll();
-//         }
-//     }
-// });
-
-// // test for listAllProfessors
-// test('listAllProfessors', async () => {
-//     const professor = await ProfessorMock.findAll();
-//     const professors = await professor.listAllProfessors();
-//     expect(professors).toEqual([professor]);
-// });
-
+    test('Should return all professors', async () => {
+        await request('localhost:3000')
+        .get('/list/professors')
+        .then(data =>{
+            console.log(data.body)
+            expect(data.body).toEqual({"professors":[{"id":1,"name":"Milos","surname":"Milosavljevic","email":"milos@gmail.com","password":"123"}]})
+        })
+    });
+});
