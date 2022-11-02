e7acf6c9242006eaeae9cd2e40ea87646d740261
Andrija Stankovic <andrijas0721@gmail.com>
2022-11-02 15:08:15 +0100

    fixed test

--- controllers/professorController.test.js
+++ controllers/professorController.test.js
@@ -1,22 +1,36 @@
-const SequelizeMock = require('sequelize-mock');
-const dbMock = new SequelizeMock();
-const professorModel = require('../models/professorModel');
+const request = require('supertest')
 
-const ProfessorMock = dbMock.define('professor', {
-    name: 'Andrija',
-    surname: 'Stankovic',
-    email: 'andrija@gmail.com',
-    password: '123456'}, {
-    instanceMethods: {
-        listAllProfessors: () => {
-            return ProfessorMock.findAll();
-        }
-    }
-});
+test('should first', async () => {
+    await request('localhost:3000')
+    .get('/list/professors')
+    .then(data =>{
+        console.log(data.statusCode)
+        expect(data.statusCode).toBe(200)
+    })
+})
 
-// test for listAllProfessors
-test('listAllProfessors', async () => {
-    const professor = await ProfessorMock.findAll();
-    const professors = await professor.listAllProfessors();
-    expect(professors).toEqual([professor]);
-});
+
+
+// const SequelizeMock = require('sequelize-mock');
+// const dbMock = new SequelizeMock();
+// const professorModel = require('../models/professorModel');
+
+// const ProfessorMock = dbMock.define('professor', {
+//     name: 'Andrija',
+//     surname: 'Stankovic',
+//     email: 'andrija@gmail.com',
+//     password: '123456'}, {
+//     instanceMethods: {
+//         listAllProfessors: () => {
+//             return ProfessorMock.findAll();
+//         }
+//     }
+// });
+
+// // test for listAllProfessors
+// test('listAllProfessors', async () => {
+//     const professor = await ProfessorMock.findAll();
+//     const professors = await professor.listAllProfessors();
+//     expect(professors).toEqual([professor]);
+// });
+
