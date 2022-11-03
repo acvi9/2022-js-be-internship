b0eeff05da87b86c2971ed124d1d3d495f1cfdb7
Mijodrag Zivkovic <mijodrag98@gmail.com>
2022-11-03 21:01:56 +0100

    professor Controller listAll functionality

---
+++ controllers/courses/courseController.js
@@ -0,0 +1,15 @@
+const Course = require('../../models/courseModel');
+const {STATUS_CODES} = require('../../constants');
+
+const listAllCourses = async (req, res) => {
+    try {
+        const courses = await Course.findAll();
+        res.status(STATUS_CODES.STATUS_OK).json({courses});
+    } catch (error) {
+        res.status(STATUS_CODES.INTERNAL_SERVER_ERROR).json(error.message);
+    }
+}
+
+module.exports = {
+    listAllCourses
+}
