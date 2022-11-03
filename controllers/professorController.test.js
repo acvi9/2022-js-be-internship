5d7e9300d80c0df034fd32487f853996c8711d6c
Andrija Stankovic <andrijas0721@gmail.com>
2022-11-03 09:24:45 +0100

    Path fix

--- controllers/professorController.test.js
+++ controllers/professorController.test.js
@@ -1,4 +1,5 @@
 const request = require('supertest');
+const server = require('../index');
 
 describe('Professor Controller', () => {
 
--- index.js
+++ index.js
@@ -6,34 +6,35 @@ const swaggerUI = require('swagger-ui-express');
 const app = express();
 const port = 3000;
 
-
+// Connecting to Database
 database.authenticate()
 	.then(() => {
-	  	console.log(`Connection has been established successfully. ${database.config.database}`);
+		console.log(`Connection has been established successfully. ${database.config.database}`);
 
 	})
 	.catch((error) => {
-	  	console.error('Unable to connect to the database: ', error);
+		console.error('Unable to connect to the database: ', error);
 	});
 
 
 
-app.get('/', (req,res)=>{
-    res.send("Hello world!");
+app.get('/', (req, res) => {
+	res.send("Hello world!");
 });
 
-app.use('/list', ProfessorRoute);
+
+app.use('/', ProfessorRoute);
 
 
-app.listen(port, ()=>{
-    console.log(`Express app is running on localhost:${port}`);
+app.listen(port, () => {
+	console.log(`Express app is running on localhost:${port}`);
 });
 
 
 
 
 const options = {
-    definition: {
+	definition: {
 		openapi: "3.0.0",
 		info: {
 			title: "College Organization API",
@@ -46,20 +47,19 @@ const options = {
 			},
 		],
 	},
-    apis: ["./index.js", "./models/professorModel.js", "./models/studentModel.js", "./models/courseModel.js", "./models/termsModel.js",
-		   "./routes/professorRoutes.js", "./routes/studentRoutes.js", "./routes/courseRoutes.js", "./routes/termsRoutes.js"],
+	apis: ["./index.js", "./models/professorModel.js", "./models/studentModel.js", "./models/courseModel.js", "./models/termsModel.js",
+		"./routes/professorRoutes.js", "./routes/studentRoutes.js", "./routes/courseRoutes.js", "./routes/termsRoutes.js"],
 }
 
 const specs = swaggerJSDoc(options);
 app.use("/docs", swaggerUI.serve, swaggerUI.setup(specs));
 
-
-
-app.get('/swagger-example', (req,res)=>{
-    res.json({
-        "id" : "a0s9djaosa",
-        "name": "name 1",
-        "exampleNumber": 2
-    });
+app.get('/swagger-example', (req, res) => {
+	res.json({
+		"id": "a0s9djaosa",
+		"name": "name 1",
+		"exampleNumber": 2
+	});
 });
 
+module.exports = express();
